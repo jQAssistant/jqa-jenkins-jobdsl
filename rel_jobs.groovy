9102ab3e837984d48f814664975bcdd1d800a2ee
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2016-10-25 23:26:23 +0200

    Script generates now also the release job for the jQA EJB 3 Plugin.

--- rel_jobs.groovy
+++ rel_jobs.groovy
@@ -25,7 +25,8 @@ def Project[] projects = [
   ["jqa-yaml-plugin", "jQA YAML Plugin", "https://github.com/buschmais/jqa-yaml-plugin.git", true],
   ["jqa-jaxrs-plugin", "jQA JAX RS Plugin", "https://github.com/buschmais/jqa-jaxrs-plugin.git", true],
   ["jqa-jpa2-plugin", "jQA JPA 2 Plugin", "https://github.com/buschmais/jqa-jpa2-plugin.git", true],
-  ["jqa-cdi-plugin", "jQA CDI Plugin", "https://github.com/buschmais/jqa-cdi-plugin.git", true]
+  ["jqa-cdi-plugin", "jQA CDI Plugin", "https://github.com/buschmais/jqa-cdi-plugin.git", true],
+  ["jqa-ejb3-plugin", "jQA EJB 3 Plugin", "https://github.com/buschmais/jqa-ejb3-plugin.git", true]
 ]
 
 projects.each {
