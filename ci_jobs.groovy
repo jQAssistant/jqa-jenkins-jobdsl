fd44d802c813606839697b873e7275166eeb319e
Dirk Mahler <dirk.mahler@buschmais.com>
2017-04-12 10:33:20 +0200

    set local repo location to LOCAL_TO_EXECUTOR

--- ci_jobs.groovy
+++ ci_jobs.groovy
@@ -91,7 +91,7 @@ def addJob(gitUrl, module, suffix, mavenGoals, upstreamJob = null) {
 	// von Artefakten in das lokale, gemeinsame Repositoy,
 	// schreiben,
 	// Oliver B. Fischer, 2017-04-12
-        //localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
+        localRepository(LocalRepositoryLocation.LOCAL_TO_EXECUTOR)
         providedSettings('Maven Settings')
         scm {
             git(gitUrl) {
