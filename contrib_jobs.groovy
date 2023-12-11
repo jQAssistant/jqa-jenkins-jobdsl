547146712625736b92be15f737026a6d06ce5570
Dirk Mahler <dirk.mahler@buschmais.com>
2023-12-11 16:02:54 +0100

    removed jQA OpenAPI plugin from Jenkins Job DSL (migrated to GH actions)

--- contrib_jobs.groovy
+++ contrib_jobs.groovy
@@ -48,7 +48,6 @@ def pluginJobs = [
         new Project(repository: 'jqassistant-m2repo-plugin'),
         new Project(repository: 'jqassistant-nexusiq-plugin'),
         new Project(repository: 'jqassistant-npm-plugin'),
-        new Project(repository: 'jqassistant-openapi-plugin', sonarTargetBranch: 'main'),
         new Project(repository: 'jqassistant-plantuml-report-plugin'),
         new Project(repository: 'jqassistant-plugin-common'),
         new Project(repository: 'jqassistant-rdbms-plugin'),
@@ -56,6 +55,7 @@ def pluginJobs = [
         new Project(repository: 'jqassistant-xmi-plugin')
 /*
         Migrated to GitHub actions:
+        new Project(repository: 'jqassistant-openapi-plugin', sonarTargetBranch: 'main'),
         new Project(repository: 'jqassistant-c4-plugin'),
         new Project(repository: 'jqassistant-typescript-plugin'),
 */
